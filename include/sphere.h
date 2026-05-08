f2e66e96881ef088dd2ea12ccc8e3c71cf260584
KaMa <maid.kanuric>
2026-05-08 09:34:27 +0200

    Add sphere object

---
+++ include/sphere.h
@@ -0,0 +1,42 @@
+/**
+ *
+ *  \file   sphere.h
+ *  \brief  Sphere class, which is a hittable object in the 3d scene.
+ *
+ */
+
+/*
+ *  Author:  KaMa (https://github.com/KaMa-0/
+ *                 https://gitlab.com/KaMa-0/)
+ *
+ */
+
+
+#ifndef _SPHERE_H
+#define _SPHERE_H
+
+#include <stdint.h>
+
+#include "hittable.h"
+#include "vec3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+typedef struct sphere {
+    hittable_t  base;
+    vec3_t      center; 
+    float       radius;
+} sphere_t;
+
+
+void sphere_init(sphere_t* s, vec3_t center, float radius);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _SPHERE_H */
---
+++ src/sphere.c
@@ -0,0 +1,52 @@
+#include <math.h>
+
+#include "sphere.h"
+
+static uint8_t
+sphere_hit(const hittable_t* self, const ray_t r, 
+           float t_min, float t_max, hit_record_t* rec)
+{
+        const sphere_t* s;
+        vec3_t          oc;
+        float           a, h, c, discriminant, sqrtd, root;
+
+        s = (const sphere_t*)self;
+        oc = vec3_sub(s->center, r.orig);
+
+        a = vec3_len_squared(r.dir);
+        h = vec3_dot(r.dir, oc);
+        c = vec3_len_squared(oc) - s->radius * s->radius;
+
+        discriminant = h * h - a * c;
+
+        if (discriminant < 0)
+                return 0; /* false */
+
+        sqrtd = sqrt(discriminant);
+
+        root = (h - sqrtd) / a;
+        if (root <= t_min || t_max <= root) {
+                root = (h + sqrtd) / a;
+                if (root <= t_min || t_max <= root)
+                        return 0; /* false */
+        }
+
+        rec->t      = root;
+        rec->p      = ray_at(r, root);
+        rec->normal = vec3_scal(vec3_sub(rec->p, s->center), 
+                                (1.0f / s->radius));
+
+        return 1; /* true */
+}
+
+static const hittable_vtable_t sphere_vtable = { 
+        .hit = sphere_hit 
+};
+
+void
+sphere_init(sphere_t* s, vec3_t center, float radius)
+{
+        s->base.vtable = &sphere_vtable;
+        s->center = center;
+        s->radius = (radius > 0) ? radius : 0;
+}
